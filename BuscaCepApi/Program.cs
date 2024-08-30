593093b89b59e3885aafd4f4acb606221f71e4cc
Talita Dias <talita.dias@itriad.org.br>
2024-08-30 13:20:07 -0400

    feat(Program): add dependency injection for IViaCepRepo and IViaCepSer

--- BuscaCepApi/Program.cs
+++ BuscaCepApi/Program.cs
@@ -1,3 +1,6 @@
+using BuscaCepApi.Repositories.Cep;
+using BuscaCepApi.Services.Cep;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<IViaCepRepository, ViaCepRepository>();
+builder.Services.AddScoped<IViaCepService, ViaCepService>();
 
 var app = builder.Build();
 
