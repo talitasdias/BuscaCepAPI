4ceac99bdc43188a24799c3939de4a28d4a94766
Talita Dias <talita.dias@itriad.org.br>
2024-08-30 12:57:18 -0400

    feat(Services/Cep): create ViaCepService class and implement iterface

---
+++ BuscaCepApi/Services/Cep/ViaCepService.cs
@@ -0,0 +1,12 @@
+﻿using BuscaCepApi.Models;
+
+namespace BuscaCepApi.Services.Cep
+{
+    public class ViaCepService : IViaCepService
+    {
+        public Task<EnderecoModel> BuscarEndereco(int cep)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
