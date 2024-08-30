f7ff19adc2f1c7067c0ff0124949ae950d3c3970
Talita Dias <talita.dias@itriad.org.br>
2024-08-30 12:47:10 -0400

    feat(Repositories/Cep): create ViaCepRepository class and implement the interface

---
+++ BuscaCepApi/Repositories/Cep/ViaCepRepository.cs
@@ -0,0 +1,12 @@
+﻿using BuscaCepApi.Models;
+
+namespace BuscaCepApi.Repositories.Cep
+{
+    public class ViaCepRepository : IViaCepRepository
+    {
+        public Task<EnderecoModel> BuscarEndereco(int cep)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
