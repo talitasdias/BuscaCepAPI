ea011e909c9a5b77921668860867bfcd012b2605
Talita Dias <talita.dias@itriad.org.br>
2024-08-30 12:59:01 -0400

    feat(Services/Cep): add constructor method

--- BuscaCepApi/Services/Cep/ViaCepService.cs
+++ BuscaCepApi/Services/Cep/ViaCepService.cs
@@ -1,9 +1,15 @@
 ﻿using BuscaCepApi.Models;
+using BuscaCepApi.Repositories.Cep;
 
 namespace BuscaCepApi.Services.Cep
 {
     public class ViaCepService : IViaCepService
     {
+        private readonly IViaCepRepository _viaCepRepository;
+        public ViaCepService(IViaCepRepository viaCepRepository)
+        {
+            _viaCepRepository = viaCepRepository;
+        }
         public Task<EnderecoModel> BuscarEndereco(int cep)
         {
             throw new NotImplementedException();
